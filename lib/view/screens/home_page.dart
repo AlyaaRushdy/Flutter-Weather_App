18431d21d213eb65d9b6cde183821e48bd52247f
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-16 05:26:13 +0200

    home and search pages

---
+++ lib/view/screens/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/view/screens/search_page.dart';
+import 'package:weather_app/view/widgets/no_result_widget.dart';
+import 'package:weather_app/view/widgets/weather_data_container.dart';
+import '../../view-models/auth_cubit.dart';
+import '../../view-models/get_weather_cubit.dart';
+import '../../view-models/weather_states.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //var cubit = AuthCubit.get(context);
+
+    return Scaffold(
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          AuthCubit.get(context).signOut(context);
+        },
+        child: const Icon(Icons.logout_rounded),
+      ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SearchPage(),
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.search_rounded,
+              size: 26,
+            ),
+          ),
+        ],
+        title: const Text("Weather App"),
+      ),
+      body: BlocBuilder<GetWeatherCubit, WeatherStates>(
+        builder: (context, state) {
+          if (state is NoWeatherState) {
+            return const NoResultWidget();
+          } else if (state is WeatherSuccessState) {
+            return WeatherDataContainer(weatherModel: state.weatherModel);
+          } else {
+            return const Center(
+              child: Text("Oops! There was an error"),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/view/screens/search_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../view-models/get_weather_cubit.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_rounded,
+            size: 26,
+          ),
+        ),
+        leadingWidth: 30,
+        title: const Text("Search a City"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: TextField(
+            onSubmitted: (cityName) async {
+              BlocProvider.of<GetWeatherCubit>(context)
+                  .getWeather(cityName: cityName);
+              Navigator.pop(context);
+            },
+            onTapOutside: (PointerDownEvent event) {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+            decoration: InputDecoration(
+              hintText: "Enter a City Name",
+              labelText: "Search",
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Theme.of(context).primaryColor,
+                  width: 1.5,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
